683a21e5ec58ceeffb8c2f29457ac5185f8bb400
Nico Prananta <nicnocquee@users.noreply.github.com>
2021-06-15 23:05:28 +0200

    remove unnecessary files

--- pages/api/monika.js
+++ pages/api/monika.js
@@ -3,8 +3,10 @@ import fs from "fs";
 import auth from "express-basic-auth";
 import initMiddleware from "../../utils/init-middleware";
 
+const user = process.env.ADMIN_USERNAME || "admin";
+const password = process.env.ADMIN_PASSWORD || "supersecret";
 const basicAuth = auth({
-  users: { [process.env.ADMIN_USERNAME]: process.env.ADMIN_PASSWORD },
+  users: { [user]: password },
   challenge: true,
 });
 
@@ -12,6 +14,7 @@ const authMiddleware = initMiddleware(basicAuth);
 
 const handler = async (req, res) => {
   await authMiddleware(req, res);
+
   const filePath = path.join(process.cwd(), "./files/monika.json");
   const content = fs.readFileSync(filePath);
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import useSWR from 'swr'
-
-const fetcher = (url) => fetch(url).then((res) => res.text())
-
 export default function Index() {
-  const { data, error } = useSWR('/api/cookies', fetcher)
-
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
-
-  return <div>{`Cookie from response: "${data}"`}</div>
+  return (
+    <div>
+      <a href="/api/monika">Download monika.json</a>
+    </div>
+  );
 }
