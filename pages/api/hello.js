320b6b467bc8277101090b6bc55955a93541d5ee
Nico Prananta <nicnocquee@users.noreply.github.com>
2021-06-15 22:54:05 +0200

    add basic auth

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,17 @@
 import path from "path";
 import fs from "fs";
+import auth from "express-basic-auth";
+import initMiddleware from "../../utils/init-middleware";
 
-const handler = (req, res) => {
+const basicAuth = auth({
+  users: { [process.env.ADMIN_USERNAME]: process.env.ADMIN_PASSWORD },
+  challenge: true,
+});
+
+const authMiddleware = initMiddleware(basicAuth);
+
+const handler = async (req, res) => {
+  await authMiddleware(req, res);
   const filePath = path.join(process.cwd(), "./files/hello.json");
   const content = fs.readFileSync(filePath);
 
