04a26b2efe3a1cf556e3b2293d17f1d573efe9c3
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-11-21 22:13:25 +0100

    dont prefetch

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Index() {
   return (
     <div>
-      <Link href="/api/download/secret.json">Download secret file</Link>
+      <Link prefetch={false} href="/api/download/secret.json">
+        Download secret file
+      </Link>
     </div>
   );
 }
